49f6fe66735cc45640beb9a149ff89f96d3c002b
3b-fly <member@3b-fly.eu>
2016-04-16 20:51:56 +0200

    Added staticclass.js JSDoc plugin
    allows to use "@class-static" tag which adds "static" doclet property

---
+++ plugins/staticclass.js
@@ -0,0 +1,12 @@
+exports.defineTags = function(dictionary) {
+  dictionary.defineTag("class-static", {
+    onTagged: function(doclet,tag) {
+      doclet.addTag('kind','class');
+      doclet.static = true;
+
+      if (tag.value && tag.value.name) {
+        doclet.addTag('name',tag.value.name);
+      }
+    }
+  });
+};
