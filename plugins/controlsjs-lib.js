ad838ecb034f732ee45632346dca5157fa4f978d
3b-fly <3b-fly@users.noreply.github.com>
2017-08-18 23:46:08 +0200

    Added @js-lib and @js-pkg tag synonyms.

--- plugins/controlsjs-lib.js
+++ plugins/controlsjs-lib.js
@@ -3,13 +3,15 @@ exports.defineTags = function(dictionary){
   dictionary.defineTag('controlsjs-library',{
     onTagged: function(doclet,tag){
       doclet.cjs_lib = tag.value;
-    }
+    },
+    synonyms: ['cjs-lib']
   });
 
   dictionary.defineTag('controlsjs-package',{
     onTagged: function(doclet,tag){
       if(!doclet.cjs_pkgs){doclet.cjs_pkgs = [];}
       doclet.cjs_pkgs.push(tag.value);
-    }
+    },
+    synonyms: ['cjs-pkg']
   });
 };
